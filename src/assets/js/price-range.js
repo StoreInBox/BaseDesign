6d9575352f0b72add7214885b5dfd03c8992ecb5
Michael Yakimenko <macgera@gmail.com>
2016-02-25 23:10:12 +0200

    Change price range, to range.

--- src/assets/js/price-range.js
+++ src/assets/js/price-range.js
@@ -1,9 +1,14 @@
 function priceRange () {
-    var slider = $('[data-role="price-range"]');
-    var sliderLabel = $('[data-role="price-range-label"]');
-    sliderLabel.html(slider.val());
-    slider.on('change', function(e) {
-      console.log(e.target.value);
-      sliderLabel.html(e.target.value);
+
+    var rangeFilter = $('[data-role="range-filter"]');
+
+    rangeFilter.each(function() {
+        var slider = $(this).find($('[data-role="range-input"]'));
+        var sliderLabel = $(this).find($('[data-role="range-label"]'));
+        sliderLabel.html(slider.val());
+        slider.on('change', function(e) {
+          console.log(e.target.value);
+          sliderLabel.html(e.target.value);
+        });
     });
 }
