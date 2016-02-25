0cacf27670df8c2d913ca5d8cc48ab7dd2c45393
Michael Yakimenko <macgera@gmail.com>
2016-02-25 23:15:16 +0200

    Fix: Remove console.log.

--- src/assets/js/range.js
+++ src/assets/js/range.js
@@ -7,7 +7,6 @@ function rangeFilter() {
         var sliderLabel = $(this).find($('[data-role="range-label"]'));
         sliderLabel.html(slider.val());
         slider.on('change', function(e) {
-          console.log(e.target.value);
           sliderLabel.html(e.target.value);
         });
     });
