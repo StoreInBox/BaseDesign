df8f16306272513baa666dda2e25810b75b09e66
Michael Yakimenko <macgera@gmail.com>
2016-02-27 16:02:51 +0200

    Set active class as variable in js script, change styles for category menu.

--- src/assets/js/categories-menu.js
+++ src/assets/js/categories-menu.js
@@ -1,13 +1,14 @@
 function CategoriesMneu() {
+    var activeClass = "catalogue-main-nav__item--active";
     var SubTabContainer = $('[data-role="sub-tab"]');
     var SubTabTrigger = $('[data-role="sub-tab-trigger"]');
     SubTabContainer.hide().filter(':first').show();
-    SubTabTrigger.filter(':first').addClass('active');
+    SubTabTrigger.filter(':first').addClass(activeClass);
     SubTabTrigger.hover(function () {
         var currentLoop = $(this).attr('sub-tabTrigger');
         SubTabContainer.hide();
         SubTabContainer.filter('[sub-tabTrigger='+currentLoop+']').show();
-        SubTabTrigger.removeClass('active');
-        $(this).addClass('active');
+        SubTabTrigger.removeClass(activeClass);
+        $(this).addClass(activeClass);
     });
 }
