5803bfe79b9c39809280905c0a5f2a8c9c915c41
Michael Yakimenko <macgera@gmail.com>
2016-03-13 15:09:56 +0200

    Basic template & scripts.

--- src/assets/js/cart-preview.js
+++ src/assets/js/cart-preview.js
@@ -1,3 +1,16 @@
 function cartPreview() {
     console.log('Init cart preview');
+    var cartPreviewBlock = $('[front-role="cart-preview"]');
+    var cartPreviewTrigger = $('[front-role="cart-preview-trigger"]');
+    var topForPagesMenuBlock = $('[front-role="main-header"]').outerHeight();
+
+    cartPreviewBlock.css({
+        'top': topForPagesMenuBlock
+    });
+
+    cartPreviewTrigger.click(function(event) {
+        event.stopPropagation();
+        cartPreviewTrigger.toggleClass('top-menu-list__link--active');
+        cartPreviewBlock.toggleClass('cart-preview--active');
+    });
 }
