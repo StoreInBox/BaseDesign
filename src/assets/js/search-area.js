f3084c791fa3a465ee1f897157448933880d4d9f
Michael Yakimenko <macgera@gmail.com>
2016-03-17 22:25:51 +0200

    Search area

--- src/assets/js/search-area.js
+++ src/assets/js/search-area.js
@@ -1,3 +1,27 @@
 function searchArea() {
-    console.log('Init search area');
+    var searchTrigger = $('[front-role="search-trigger"]');
+    var searchArea = $('[front-role="search-area"]');
+    var close = $('[front-role="search-trigger-close-icon"]');
+
+    var body = $('body');
+
+    var triggerPositionLeft = searchTrigger.offset().left;
+    var triggerPositionTop = searchTrigger.offset().top;
+
+    searchTrigger.css({
+        "top": triggerPositionTop,
+        "left": triggerPositionLeft
+    });
+
+    searchTrigger.click(function(e) {
+        e.stopPropagation();
+        searchTrigger.toggleClass('search__trigger--active');
+        searchArea.toggleClass('search-area--active');
+        body.toggleClass('no-scroll');
+    });
+    close.click(function() {
+        searchTrigger.toggleClass('search__trigger--active');
+        searchArea.toggleClass('search-area--active');
+        body.toggleClass('no-scroll');
+    });
 }
