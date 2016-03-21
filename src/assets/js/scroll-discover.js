7294d8f1e1fdd10ba241efae183e27c264c34d29
Michael Yakimenko <macgera@gmail.com>
2016-03-21 16:28:32 +0200

    Scroll to content

--- src/assets/js/scroll-discover.js
+++ src/assets/js/scroll-discover.js
@@ -1,6 +1,6 @@
 // scroll by click discover more
 function scrollToContent() {
-    var trigger = $('[data-front="scroll-to-content"]');
+    var trigger = $('[front-role="scroll-to-content"]');
     var windowHeight = $(window).height();
     var panelHeight = $('[front-role="base-header"]').height() + 10;
     trigger.click(function() {
