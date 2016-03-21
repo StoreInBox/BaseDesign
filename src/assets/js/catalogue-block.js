49b8765046a463570b02801ffb7b1ae5ebba3fd5
Michael Yakimenko <macgera@gmail.com>
2016-03-21 16:57:00 +0200

    Fix width for catalogue.

--- src/assets/js/catalogue-block.js
+++ src/assets/js/catalogue-block.js
@@ -1,23 +1,12 @@
 function catalogueBlock() {
-    var mobileMedia = window.matchMedia("(max-width : 992px)").matches;
     var catalogueBlock = $('[front-role="catalogue-block"]');
     var topForBlock = $('[front-role="main-header"]').outerHeight();
     var catalogueBlockTrigger = $('[front-role="catalogue-block-trigger"]');
     var catalogueBlockTriggerOffset = catalogueBlockTrigger.offset().left;
 
-    if (!mobileMedia) {
-        catalogueBlock.css({
-            'top': topForBlock,
-            'padding-right': catalogueBlockTriggerOffset,
-            'padding-left': catalogueBlockTriggerOffset
-        });
-    } else {
-        catalogueBlock.css({
-            'top': topForBlock,
-            'padding-right': 0,
-            'padding-left': 0
-        });
-    }
+    catalogueBlock.css({
+        'top': topForBlock + 1,
+    });
 
     catalogueBlockTrigger.click(function(event) {
         event.stopPropagation();
